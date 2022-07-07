2ca5155544832912f12993f1149e04f27f35ac1d
zamama-zaman <zamamazaman@gmail.com>
2022-07-07 13:53:44 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/ugks_limousine_driver_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ugks_limousine_driver_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:ugks_limousine_driver_app/presentation/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UGKS Limousine Driver App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
